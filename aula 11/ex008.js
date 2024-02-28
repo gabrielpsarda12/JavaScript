72092c34137418efc69faa4245ed20ce70f3413a
gabrielpsarda12 <sarda.gabriel@gmail.com>
2024-02-28 12:32:52 -0300

    estudando

---
+++ aula 11/ex008.js
@@ -0,0 +1,7 @@
+let vel = 62
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { // Condição Simples
+    console.log(`Voce ultrapassou a vel max. MULTADO`)
+}
+
+console.log(`Dirija sempre com cinto de segurança`)
---
+++ aula 11/ex009.js
@@ -0,0 +1,6 @@
+let pais = 'BRA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+} else { // Condição composta
+    console.log(`Estrangeiro`)
+}
